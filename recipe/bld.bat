54884501d7be836d2542fcc8cf539ba4bb05dea4
Benjamin Bertrand <benjamin.bertrand@maxiv.lu.se>
2025-07-14 09:53:55 +0200

    Update aggregate-third-party-report path

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -7,7 +7,7 @@ call mvn install -DskipTests
 
 REM Generate third party license report
 call mvn license:aggregate-third-party-report
-copy target\site\aggregate-third-party-report.html .
+copy target\reports\aggregate-third-party-report.html .
 
 mkdir "%PREFIX%\share\java"
 copy org.tango.pogo.gui\target\Pogo-%PKG_VERSION%.jar %PREFIX%\share\java\Pogo.jar
